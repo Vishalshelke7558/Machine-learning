f9e14082630612924c0f87f9e7379bef3f185a9e
Vishal Shelke <86156694+Vishalshelke7558@users.noreply.github.com>
2021-08-19 23:18:23 +0530

    Add files via upload

---
+++ basics_of_R_lang.R
@@ -0,0 +1,31 @@
+install.packages("LiblineaR") # command for installing packges
+
+library() # to check the installed libraries
+
+search() # to check the library is loaded or not
+
+require("LiblineaR")  # to load the library
+
+search()
+
+? LiblineaR # to know more about library
+
+? ggplot2
+
+require("ggplot2")
+
+? ggplot2
+
+data() # to check avilable datasets
+
+library(help = "datasets") 
+
+? iris # to know more about dataser
+
+search()
+
+str(iris) # to know the structure of the dataset
+
+iris # to see total dataset
+
+data(iris) # to load the dataset
